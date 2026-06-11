5648b53970f200fc680c1c63bcd47710398f4613
AliffAzlan <aliffazlan0511@gmail.com>
2026-06-11 20:18:42 +1000

    server port

--- server.js
+++ server.js
@@ -120,6 +120,6 @@ const server = http.createServer(async (req, res) => {
   }
 });
 
-server.listen(PORT, () => {
+server.listen(PORT, "127.0.0.1",() => {
   console.log(`Sydney quiz server running at http://localhost:${PORT}`);
 });
